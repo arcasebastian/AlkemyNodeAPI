c73ed9bbb232c1f502a9f8e5dba9cc7ed3917eb2
sebastian <sebasarca711@gmail.com>
2021-10-07 18:01:11 -0300

    Genres implementation

--- controllers/genres.js
+++ controllers/genres.js
@@ -1,22 +1,76 @@
-exports.post = (req, res, next) => {
-  return res.status(201).json({ status: "New genre successfully created" });
+const { validationResult } = require("express-validator");
+const Genre = require("../models/Genre");
+const { normalizeError } = require("../util/normalizeError");
+
+exports.post = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    const { name } = req.body;
+    if (!req.file) {
+      return next(normalizeError("Image is required", 400));
+    }
+    const imageFile = req.file.filename;
+    const newGenre = new Genre({
+      name: name,
+      image: `/images/${imageFile}`,
+    });
+    if (await newGenre.save()) {
+      return res.status(201).json({ status: "New genre successfully created" });
+    }
+  } else {
+    let firstError = errors.array({ onlyFirstError: true })[0];
+    let errorCode = firstError.msg.includes("already") ? 409 : 400;
+    return next(normalizeError(firstError.msg, errorCode));
+  }
 };
-exports.getAll = (req, res, next) => {
-  return res
-    .status(200)
-    .json([
-      { name: "Animation", image: "https://localhost/images/dhdwu191832h" },
-    ]);
+
+exports.getAll = async (req, res, next) => {
+  const genres = await Genre.getAll();
+  res.status(200).json(genres);
 };
-exports.getOne = (req, res, next) => {
-  return res.status(200).json({
-    name: "Animation",
-    image: "https://localhost/images/dhdwu191832h",
-  });
+exports.getOne = async (req, res, next) => {
+  const requestedId = req.params.id;
+  const genre = await Genre.getOne(requestedId);
+  if (genre) return res.status(200).json(genre);
+  else return next(normalizeError("Not found", 404));
 };
-exports.put = (req, res, next) => {
-  return res.status(200).json({ status: "Genre successfully updated" });
+exports.put = async (req, res, next) => {
+  const requestedId = req.params.id;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    let firstError = errors.array({ onlyFirstError: true })[0];
+    return next(normalizeError(firstError.msg, 400));
+  } else {
+    try {
+      const genreToUpdate = await Genre.getOne(requestedId);
+      if (!genreToUpdate) return next(normalizeError("Not found", 404));
+      const { name } = req.body;
+      const imageFile = req.file.filename;
+      if (!imageFile) return next(normalizeError("Invalid file", 400));
+      genreToUpdate.name = name;
+      genreToUpdate.image = `/images/${imageFile}`;
+      if (await genreToUpdate.save())
+        return res.status(200).json({ status: "Genre successfully updated" });
+    } catch (err) {
+      console.log(err);
+      return next(normalizeError(err.message, 500));
+    }
+  }
 };
-exports.delete = (req, res, next) => {
-  return res.status(200).json({ status: "Genre was successfully deleted" });
+exports.delete = async (req, res, next) => {
+  const requestedId = req.params.id;
+  try {
+    const genreToUpdate = await Genre.getOne(requestedId);
+    if (!genreToUpdate) {
+      return next(normalizeError("Not found", 404));
+    }
+    const movies = await genreToUpdate.countMovies();
+    if (movies > 0) {
+      return next(normalizeError("Genre cant be deleted"));
+    }
+    await genreToUpdate.destroy();
+    return res.status(200).json({ status: "Genre was successfully deleted" });
+  } catch (err) {
+    return next(normalizeError(err.message, 500));
+  }
 };
--- routes/genres.js
+++ routes/genres.js
@@ -1,11 +1,36 @@
 const express = require("express");
 const controller = require("../controllers/genres");
-
+const { body } = require("express-validator");
+const Genre = require("../models/Genre");
 const router = express.Router();
-
-router.post("/", controller.post);
+router.post(
+  "/",
+  [
+    body("name")
+      .trim()
+      .isLength({ min: 3 })
+      .withMessage("A genre name is required")
+      .custom((value, { req }) => {
+        return Genre.findOne({ where: { name: value } }).then((genre) => {
+          console.log(value);
+          if (genre) return Promise.reject("Genre is already registered");
+          return Promise.resolve();
+        });
+      }),
+  ],
+  controller.post
+);
 router.get("/", controller.getAll);
 router.get("/:id", controller.getOne);
-router.put("/:id", controller.put);
+router.put(
+  "/:id",
+  [
+    body("name")
+      .trim()
+      .isLength({ min: 3 })
+      .withMessage("A genre name is required"),
+  ],
+  controller.put
+);
 router.delete("/:id", controller.delete);
 module.exports = router;
