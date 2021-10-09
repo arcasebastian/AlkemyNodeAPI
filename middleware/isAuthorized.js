2d22ab0feb03ac33683a0af7fe793bdcc4c57ffd
sebastian <sebasarca711@gmail.com>
2021-10-09 01:37:13 -0300

    Fixing unathorized mistake

--- middleware/isAuthorized.js
+++ middleware/isAuthorized.js
@@ -4,17 +4,17 @@ const { normalizeError } = require("../util/normalizeError");
 exports.isAuth = (req, res, next) => {
   const authorization = req.get("Authorization");
   if (!authorization) {
-    return next(normalizeError("Not authorized", 405));
+    return next(normalizeError("Not authorized", 401));
   }
   const token = authorization.split(" ")[1];
   let decodedAuthToken;
   try {
     decodedAuthToken = jwt.verify(token, env.jwtSecret);
   } catch (err) {
-    return next(normalizeError("Not authorized", 405));
+    return next(normalizeError("Not authorized", 401));
   }
   if (!decodedAuthToken) {
-    return next(normalizeError("Not authorized", 405));
+    return next(normalizeError("Not authorized", 401));
   }
   req.userId = decodedAuthToken.id;
   return next();
